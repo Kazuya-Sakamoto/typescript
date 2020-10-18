6efadbdfde1846d16fcc343abee3d8dbd128e559
坂本一弥 <sskazuya1130@gmail.com>
2020-10-18 21:25:23 +0900

    Tuple, Enum

--- Type/index.js
+++ Type/index.js
@@ -13,3 +13,24 @@ var person = {
     age: 24
 };
 console.log(person.age);
+// * 配列
+var fruits = ['Apple', 'Banana', 'Grape', 1];
+var fruit = fruits[0];
+console.log(fruits);
+// * Tuple型 型の順序が決めたい場合
+var book = ['business', 1500, false];
+book.push(21); // ? Errorが出ない
+// console.log(book[3]); //? Error を出すよ 
+// * Enum 列挙型 特定のまとまったグループを宣言する
+var coffeeSize;
+(function (coffeeSize) {
+    coffeeSize["SHORT"] = "SHORT";
+    coffeeSize["TALL"] = "TALL";
+    coffeeSize["GRANDE"] = "GRANDE";
+    coffeeSize["VENTI"] = "VENTI";
+})(coffeeSize || (coffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: coffeeSize.TALL
+};
+coffee.size = coffeeSize.SHORT;
--- Type/index.ts
+++ Type/index.ts
@@ -19,3 +19,33 @@ const person: {
 }
 console.log(person.age);
 
+// * 配列
+const fruits = ['Apple', 'Banana', 'Grape', 1];
+const fruit = fruits[0];
+console.log(fruits);
+
+// * Tuple型 型の順序が決めたい場合
+const book: [string, number, boolean ] = ['business', 1500, false];
+book.push(21) // ? Errorが出ない
+// console.log(book[3]); //? Error を出すよ 
+
+// * Enum 列挙型 特定のまとまったグループを宣言する パスカルケースで記載
+// enum CoffeeSize {
+//   SHORT = 'SHORT',
+//   TALL = 'TALL',
+//   GRANDE = 'GRANDE',
+//   VENTI = 'VENTI'
+// }
+// ? 数字が入る
+enum CoffeeSize {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI 
+}
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL
+}
+coffee.size = CoffeeSize.SHORT;
+
