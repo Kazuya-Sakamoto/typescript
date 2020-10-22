fd7cc48452ee1413df7a6730ed608f9f359c8b3d
坂本一弥 <sskazuya1130@gmail.com>
2020-10-22 21:46:47 +0900

    & を使用

---
+++ src/advanced.ts
@@ -0,0 +1,66 @@
+interface Engineer {
+  name: string;
+  role: string;
+}
+
+interface Blogger {
+  name: string;
+  follower: number;
+}
+// * & を使用
+// type EngineerBlogger = Engineer & Blogger;
+interface EngineerBlogger extends Engineer, Blogger {}
+
+const quill: EngineerBlogger = {
+  name: 'Quill',
+  role: 'front-end',
+  follower: 1000
+}
+
+type NumberBoolean = number | boolean;
+type StringNumber = string | number;
+type Mix = NumberBoolean & StringNumber;
+
+
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase();
+  } 
+  return ''
+}
+
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name)
+  if ('role' in nomadWorker) { //? in演算子
+    console.log(nomadWorker.role)
+  } 
+  if ('follower' in nomadWorker) { //? in演算子
+    console.log(nomadWorker.follower)
+  } 
+}
+
+class Dog {
+  speak() {
+    console.log('bow-bow');
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('tweet-tweet')
+  }
+  fly() {
+    console.log('flutter')
+  }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+
+havePet(new Bird());
