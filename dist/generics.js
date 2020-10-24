dde79ec4c86be485270feb3323abcbc162296f15
坂本一弥 <sskazuya1130@gmail.com>
2020-10-24 11:49:05 +0900

    class ジェネリック

--- dist/generics.js
+++ dist/generics.js
@@ -1,7 +1,27 @@
 "use strict";
 // * generics <T> 型を新しく作成する
 // * extends で 型を指定する
-function copy(value) {
+function copy(value, key) {
     return value;
 }
-console.log(copy({ name: 'Taro' }));
+console.log(copy({ name: 'Taro', age: 35 }, 'age'));
+class LightDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase);
--- src/TypeScript/generics.ts
+++ src/TypeScript/generics.ts
@@ -1,7 +1,26 @@
 // * generics <T> 型を新しく作成する
 // * extends で 型を指定する
-function copy<T extends { name: string }>(value: T): T {
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T {
   return value;
 }
-console.log(copy({ name: 'Taro' }));
+console.log(copy({ name: 'Taro', age: 35 }, 'age'));
 
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase);
