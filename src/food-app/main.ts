722fc12210e5d91faad728c8a9e90490a35a5fce
坂本一弥 <sskazuya1130@gmail.com>
2020-10-25 01:39:58 +0900

    webpack publicPath

--- src/food-app/main.ts
+++ src/food-app/main.ts
@@ -3,5 +3,5 @@ import { Foods } from './foods';
 // import { Foods as FoodsList } from './foods.js'; //? asを使用して命名を変える
 // import * as Foods from './foods.js'; //? 全部 取得する
 // import fafafa from './foods.js'; //? export default と同時に使う 命名はなんでもよくなる
-
+console.log('Hello')
 Foods.getInstance();
