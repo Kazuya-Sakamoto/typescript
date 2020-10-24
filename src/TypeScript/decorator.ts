e82a1b2b064678ac9f953266ec655b146adb8e8a
坂本一弥 <sskazuya1130@gmail.com>
2020-10-24 14:35:53 +0900

    デコレーター es7から導入予定

---
+++ src/TypeScript/decorator.ts
@@ -0,0 +1,13 @@
+function Logging(target: Function) {
+  console.log('Logging...')
+  console.log(constructor);
+}
+
+// * インスタンス生成時 ではなく class定義時に読まれる
+@Logging
+class User {
+  name = 'Quill';
+  constructor() {
+    console.log('User was created!')
+  }
+}
