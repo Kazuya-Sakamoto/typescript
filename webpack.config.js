6e65748d49dbffb123595f51d77c58bb3983163c
坂本一弥 <sskazuya1130@gmail.com>
2020-10-24 20:49:46 +0900

    ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/main.js', //? ここからStartする
+  entry: './src/food-app/main.ts', //? ここからStartする
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   }
 }
